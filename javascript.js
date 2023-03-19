e435b0105e6530fd8f0a6b6450747f178b7616bf
Ekansh Goel <ekanshgoel123@gmail.com>
2023-03-18 20:25:15 -0400

    complete
    completed, all the images are added

---
+++ javascript.js
@@ -0,0 +1,13 @@
+
+var btn = document.getElementById("checkbtn");
+
+
+
+btn.addEventListener("checkbtn", function (event) 
+{
+
+    var name = document.getElementById("name");
+    var date = document.getElementById("date");
+    alert(name);
+    
+});
